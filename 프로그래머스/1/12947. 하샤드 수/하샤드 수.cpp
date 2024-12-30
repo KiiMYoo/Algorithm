9a6847f741d9263da996b7dba62c835d6f681bea
KiiMYoo <102599100+KiiMYoo@users.noreply.github.com>
2024-12-30 15:10:58 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -5,18 +5,11 @@ using namespace std;
 
 
 bool solution(int x) {
-    bool answer = true;
     int a = 0;
     int initial_x = x;
     while(x!=0){
         a += x%10;
         x /= 10;
     }
-    if (initial_x % a == 0){
-        answer = true;
-    }
-    else{
-        answer = false;
-    }
-    return answer;
+    return initial_x%a == 0;
 }
