a56b174b6663cc09280f84caae7d911d0afe9da5
KiiMYoo <102599100+KiiMYoo@users.noreply.github.com>
2025-01-22 09:26:50 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.03 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    for(int i = 1; i < food.size() ; i++)
+    {
+        answer.append(food[i]/2, i+48);
+    }
+    
+    answer += '0';
+    
+    for(int i = food.size()-1; i > 0  ; i--)
+    {
+        answer.append(food[i]/2, i+48);
+    }
+    return answer;
+}
