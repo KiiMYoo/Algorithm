6f5e8c788f2b081bcebc6752789eb16484fbc7a4
Kim Yeon Jun <102599100+KiiMYoo@users.noreply.github.com>
2025-02-07 10:57:39 +0900

    [level 1] Title: 옹알이 (2), Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -9,16 +9,36 @@ int solution(vector<string> babbling)
 
     for(string s : babbling)
     {
-        int flag=0; bool j=true;
+        int flag=0; 
+        bool j=true;
+        
         for(int i=0; i<s.length(); i++)
         {
-            if(s.substr(i,3)=="aya" && flag!=1) { flag=1; i+=2; }
-            else if(s.substr(i,2)=="ma" && flag!=2) { flag=2; i+=1; }
-            else if(s.substr(i,3)=="woo" & flag!=3) { flag=3; i+=2; }
-            else if(s.substr(i,2)=="ye" && flag!=4) { flag=4; i+=1; }
-            else { j=false; break; }
+            if(s.substr(i,3)=="aya" && flag!=1) 
+            { 
+                flag=1; i+=2; 
+            }
+            else if(s.substr(i,2)=="ma" && flag!=2) 
+            { 
+                flag=2; i+=1; 
+            }
+            else if(s.substr(i,3)=="woo" & flag!=3) 
+            { 
+                flag=3; i+=2; 
+            }
+            else if(s.substr(i,2)=="ye" && flag!=4) 
+            { 
+                flag=4; i+=1; 
+            }
+            else 
+            { 
+                j=false; break; 
+            }
         }
-        if(j==true) answer++;
+        if(j==true) 
+        {
+            answer++;
+        }
     }
 
     return answer;
