44e55726e194ec0491489ab06d168f9ad11b1dfd
Kim Yeon Jun <102599100+KiiMYoo@users.noreply.github.com>
2025-02-13 17:45:48 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.18 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<string> keymap, vector<string> targets) {
+    vector<int> answer;
+    for(int k = 0; k < targets.size(); k ++)
+    {
+        int LeastPushSum = 0;
+        for(int i  = 0; i < targets[k].length(); i++)
+        {
+            vector<int> Pushes;
+            int LeastPush = 2147483647;
+            for(int j = 0; j < keymap.size(); j++)
+            {
+                auto it = keymap[j].find(targets[k][i]);
+                if(it != string::npos && LeastPush > it)
+                {
+                    LeastPush = it;
+                }
+            }
+            if(LeastPush != 2147483647)
+            {
+                LeastPushSum += LeastPush + 1;
+            }
+            else
+            {
+                LeastPushSum = -1;
+                break;
+            }
+        }
+        answer.push_back(LeastPushSum);
+    }
+   
+    return answer;
+}
