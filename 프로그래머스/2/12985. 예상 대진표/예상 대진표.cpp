6b0dfba320957deea5633b580c41d8700bf8196c
Kim Yeon Jun <102599100+KiiMYoo@users.noreply.github.com>
2025-03-10 09:23:51 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+    
+    while( n != 0)
+    {
+        if (a % 2 == 0 && b % 2 != 0)
+        {
+            if(a - 1 != b)
+            {
+                n /= 2;
+                a = a/2;
+                b = b/2 + 1;
+                answer++;
+            }
+            else
+            {
+                answer++;
+                return answer;
+            }
+        }
+        if (a % 2 != 0 && b % 2 == 0)
+        {
+            if(b - 1 != a)
+            {
+                n /= 2;
+                a = a/2 + 1;
+                b = b/2;
+                answer++;
+            }
+            else
+            {
+                answer++;
+                return answer;
+            }
+        }
+        if (a % 2 == 0 && b % 2 == 0)
+        {
+            n /= 2;
+            a = a/2;
+            b = b/2;
+            answer++;
+        }
+        if (a % 2 != 0 && b % 2 != 0)
+        {
+            n /= 2;
+            a = a/2 + 1;
+            b = b/2 + 1;
+            answer++;
+        }
+    }
+    
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
